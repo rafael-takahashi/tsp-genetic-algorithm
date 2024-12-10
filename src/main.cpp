07ab2f099e4f208582b9bf4e2152e1ecd6ea5956
Rafael Takahashi <rafael.takahashi20@outlook.com>
2024-12-10 11:11:05 -0300

    chore: add chrono library to track time efficiency

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,9 @@
+#include <chrono>
 #include "tsp.h"
 
 using namespace std;
 
 int main() {
-
-    /*
-        -gr666.tsp
-        pcb1173.tsp
-        pr1002.tsp
-        brd14051.tsp
-        fnl4461.tsp
-        d15112.tsp
-        pla33810.tsp
-        pla85900.tsp
-    */
-
     vector<string> instances_file_paths = {
         "instances/att532.tsp",
         "instances/pcb1173.tsp",
@@ -26,18 +15,30 @@ int main() {
         "instances/pla85900.tsp"
     };
 
-    //for (size_t i = 0; i < instances_file_paths.size(); i++) {
-        string file_path = "instances/brd14051.tsp";
+    for (size_t i = 0; i < instances_file_paths.size(); i++) {
+        string file_path = instances_file_paths[i];
         cout << "--------------------------------\nFile path: " << file_path << "\n";
 
         vector<Node> node_list = tsp_to_vector(file_path);
 
+        auto neighbor_begin = chrono::high_resolution_clock::now();
         Path nearest_neighbor_path = nearest_neighbor(node_list);
-        cout << "\nNearest neighbor traveled distance: " << nearest_neighbor_path.distance << "\n";
+        auto neighbor_end = chrono::high_resolution_clock::now();
 
+        cout << "\nNearest neighbor traveled distance: " << nearest_neighbor_path.distance << "\n";
+        cout << "Nearest neighbor execution time: " 
+             << chrono::duration_cast<chrono::milliseconds>(neighbor_end - neighbor_begin).count() 
+             << "ms\n";
+
+        auto two_opt_begin = chrono::high_resolution_clock::now();
         Path two_opt_path = two_opt(nearest_neighbor_path);
-        cout << "\n2-opt traveled distance: " << two_opt_path.distance << "\n--------------------------------\n";
-    //}
-    
+        auto two_opt_end = chrono::high_resolution_clock::now();
+
+        cout << "\n2-opt traveled distance: " << two_opt_path.distance << '\n';
+        cout << "Nearest neighbor execution time: " 
+             << chrono::duration_cast<chrono::milliseconds>(neighbor_end - neighbor_begin).count() 
+             << "ms\n";
+    }
+    cout << "--------------------------------\n";
     return 0; 
 }
