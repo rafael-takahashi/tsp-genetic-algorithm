d83ab39e8dffed0a603ac93d0a5c7611226b629c
Rafael Takahashi <rafael.takahashi20@outlook.com>
2025-01-13 19:16:12 -0300

    feat: add seed as parameter

--- src/parameters.cpp
+++ src/parameters.cpp
@@ -1,4 +1,5 @@
 #include "parameters.h"
 
+const int SEED = 42;
 const int POPULATION_SIZE = 50;
 const float MUTATION_RATE = 0.05;
--- src/parameters.h
+++ src/parameters.h
@@ -1,4 +1,5 @@
 #pragma once
 
+extern const int SEED;
 extern const int POPULATION_SIZE;
 extern const float MUTATION_RATE;
