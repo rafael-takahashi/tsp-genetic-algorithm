93e7a6a0230270d8ec39ed39173b9b0adab61428
Rafael Takahashi <rafael.takahashi20@outlook.com>
2025-09-10 08:33:23 -0300

    feat: parallelize ga pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "tsp.h"
 #include "genetic.h"
 #include "parameters.h"
+#include "ThreadPool.h"
 
 using namespace std;
 
@@ -27,34 +28,60 @@ int main() {
      cout << fixed << setprecision(15) << "Average distance: " << average << "\n";
 
      int generation = 0;
-     int iterations_without_improvement = 0;
      double new_average = 0.0;
+
+     ThreadPool pool;
+     
+     int total_to_generate = POPULATION_SIZE - ELITE_SIZE;
+     int segment_size = (total_to_generate) / MAX_THREADS;
+     int remainder = (total_to_generate) % MAX_THREADS;
 
      while (generation != MAX_GENERATIONS) {
           vector<Path> new_population;
           new_population.reserve(POPULATION_SIZE);
-
+          
           vector<int> elite_indexes = get_elite_indexes(population);
           for (int idx : elite_indexes)
-               new_population.push_back(population[idx]);
+          new_population.push_back(population[idx]);
+          
+          vector<future<vector<Path>>> futures;
+          futures.reserve(MAX_THREADS);
 
-          double average_fitness = calculate_fitness(average);
+          for (int t = 0; t < MAX_THREADS; t++) {
+               int this_segment = segment_size + (t < remainder ? 1 : 0);
 
-          while (new_population.size() < POPULATION_SIZE) {
-               auto [parent1, parent2] = tournament_selection(population, gen);
+               futures.push_back(move(pool.enqueue([&, this_segment](mt19937& rng) {
+                    vector<Path> partial_population;
+                    partial_population.reserve(this_segment);
 
-               auto [offspring1, offspring2] =
-                    order_crossover(parent1.node_sequence, parent2.node_sequence, gen);
+                    uniform_real_distribution<double> prob_dist(0.0, 1.0);
 
-               if (prob_dist(gen) < MUTATION_RATE) swap_mutation(offspring1, gen);
-               if (prob_dist(gen) < MUTATION_RATE) swap_mutation(offspring2, gen);
+                    while ((int)partial_population.size() < this_segment) {
+                         auto [parent1, parent2] = tournament_selection(population, rng);
 
-               if (prob_dist(gen) < LOCAL_OPT_RATE) offspring1 = two_opt(offspring1);
-               if (prob_dist(gen) < LOCAL_OPT_RATE) offspring2 = two_opt(offspring2);
+                         auto [offspring1, offspring2] =
+                              order_crossover(parent1.node_sequence, parent2.node_sequence, rng);
 
-               new_population.push_back(offspring1);
-               if (new_population.size() < POPULATION_SIZE)
-                    new_population.push_back(offspring2);
+                         if (prob_dist(rng) < MUTATION_RATE) swap_mutation(offspring1, rng);
+                         if (prob_dist(rng) < MUTATION_RATE) swap_mutation(offspring2, rng);
+
+                         if (prob_dist(rng) < LOCAL_OPT_RATE) offspring1 = two_opt(offspring1);
+                         if (prob_dist(rng) < LOCAL_OPT_RATE) offspring2 = two_opt(offspring2);
+
+                         partial_population.push_back(move(offspring1));
+                         if ((int)partial_population.size() < this_segment)
+                              partial_population.push_back(move(offspring2));
+                    }
+
+                    return partial_population;
+               })));
+          }
+
+          for (auto& fut : futures) {
+               auto part = fut.get();
+               new_population.insert(new_population.end(),
+                                   make_move_iterator(part.begin()),
+                                   make_move_iterator(part.end()));
           }
 
           population = move(new_population);
